17381031b89f33434c0de66ccb4f060fe9bbdf9a
movingpictures83 <movingpictures83@gmail.com>
2021-01-27 19:37:32 -0500

    Using synch and asynch

--- testbench.v
+++ testbench.v
@@ -1,24 +1,48 @@
-`timescale 1ns / 1ps
+//`timescale 1ns / 1ps
 module testbench();
 
+reg clk;
 reg[31:0] addr;
-wire[31:0] readdata;
+wire[31:0] readdataSynch;
+wire[31:0] readdataAsynch;
 reg[31:0] writedata;
 reg we;
 
-DM DMemory(addr, readdata, writedata, we);
+DM_synch DMemorySynch(clk, addr, readdataSynch, writedata, we);
+DM_asynch DMemoryAsynch(addr, readdataAsynch, writedata, we);
 
 
+localparam CLK_PERIOD=20;
+
+always @*
+begin
+   clk <= 1;  # (CLK_PERIOD/2);
+   clk <= 0;  # (CLK_PERIOD/2);
+end
 
 initial begin
-$readmemh("datamem.dat",DMemory.memory);
+$readmemh("datamem.dat",DMemorySynch.memory);
+$readmemh("datamem.dat",DMemoryAsynch.memory);
 addr=8; we=0; writedata=42; #10; 
-if (readdata !== 2) begin
-           $display("Error: Incorrect data read at address 8: Expected 2 got %d", readdata); $stop;
+if (readdataAsynch !== 2) begin
+           $display("[SYNCH] Error: Incorrect data read at address 8: Expected 2 got %d", readdataSynch); $stop;
+end
+if (readdataSynch !== 2) begin
+           $display("[ASYNCH] Error: Incorrect data read at address 8: Expected 2 got %d", readdataAsynch); $stop;
 end
 we=1;  #10;
-if (readdata !== 42) begin
-    $display("Error: Incorrect data read at address 8: Expected 42 got %d", readdata); $stop;
+if (readdataAsynch !== 42) begin
+    $display("[SYNCH] Error: Incorrect data read at address 8: Expected 42 got %d", readdataSynch); $stop;
+end
+if (readdataSynch !== 2) begin
+    $display("[ASYNCH] Error: Incorrect data read at address 8: Expected 2 got %d", readdataAsynch); $stop;
+end
+#10;
+if (readdataAsynch !== 42) begin
+    $display("[SYNCH] Error: Incorrect data read at address 8: Expected 42 got %d", readdataSynch); $stop;
+end
+if (readdataSynch !== 42) begin
+    $display("[ASYNCH] Error: Incorrect data read at address 8: Expected 42 got %d", readdataAsynch); $stop;
 end
 $display("All tests passed.");
 end
