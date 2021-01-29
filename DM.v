b922b622ec0a8d8ba13e16671cd9ec408a7ca47e
movingpictures83 <movingpictures83@gmail.com>
2021-01-29 09:14:13 -0500

    Changed names

--- DM.v
+++ DM.v
@@ -1,4 +1,24 @@
 `timescale 1ns / 1ps
+
+module DM_synch(clk, a, rd, wd, we); 
+   input clk;   
+   input[31:0] a;
+   input we;
+   output wire [31:0] rd;
+   input [31:0] wd;
+   
+   reg [31:0] memory [0:1023];
+   
+   always @(clk) begin
+     if (we) begin
+         memory[a >> 2] = wd;
+      end
+   end
+   
+   assign rd = memory[a >> 2];
+endmodule
+
+
 module DM(a, rd, wd, we);    
    input[31:0] a;
    input we;
