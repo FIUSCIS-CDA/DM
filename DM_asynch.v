91f4c48333c471f58f57f5ff561487483bd2b758
movingpictures83 <movingpictures83@gmail.com>
2021-02-02 06:42:21 -0500

    Asynchronous and synchronous

---
+++ DM_asynch.v
@@ -0,0 +1,17 @@
+
+module DM_asynch(a, rd, wd, we);    
+   input[31:0] a;
+   input we;
+   output wire [31:0] rd;
+   input [31:0] wd;
+   
+   reg [31:0] memory [0:1023];
+   
+   always @(we) begin
+     if (we) begin
+         memory[a >> 2] = wd;
+      end
+   end
+   
+   assign rd = memory[a >> 2];
+endmodule
---
+++ DM_synch.v
@@ -0,0 +1,19 @@
+//`timescale 1ns / 1ps
+`timescale 100ps / 1ps
+module DM_synch(clk, a, rd, wd, we); 
+   input clk;   
+   input[31:0] a;
+   input we;
+   output wire [31:0] rd;
+   input [31:0] wd;
+   
+   reg [31:0] memory [0:1023];
+   
+   always @(clk) begin
+     if (we) begin
+         memory[a >> 2] = wd;
+      end
+   end
+   
+   assign rd = memory[a >> 2];
+endmodule
