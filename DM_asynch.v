0ae41b74d3b24a6bc42463f38bf0f2f7999ae7b0
movingpictures83 <movingpictures83@gmail.com>
2021-02-06 15:49:36 -0500

    Fixed to use latencies

--- DM_asynch.v
+++ DM_asynch.v
@@ -6,12 +6,15 @@ module DM_asynch(a, rd, wd, we);
    input [31:0] wd;
    
    reg [31:0] memory [0:1023];
-   
-   always @(we) begin
+   reg [31:0] buffer;   
+
+   always @(*) begin
+     #90;
      if (we) begin
          memory[a >> 2] = wd;
       end
+     buffer = memory[a >> 2];
    end
    
-   assign rd = memory[a >> 2];
+   assign rd = buffer;//memory[a >> 2];
 endmodule
--- DM_synch.v
+++ DM_synch.v
@@ -1,5 +1,4 @@
-//`timescale 1ns / 1ps
-//`timescale 100ps / 1ps
+
 module DM_synch(clk, a, rd, wd, we); 
    input clk;   
    input[31:0] a;
@@ -8,12 +7,16 @@ module DM_synch(clk, a, rd, wd, we);
    input [31:0] wd;
    
    reg [31:0] memory [0:1023];
-   
+   //reg [31:0] buffer;   
+
    always @(clk) begin
+    // #90;
+
      if (we) begin
          memory[a >> 2] = wd;
       end
+     //buffer = memory[a >> 2];
    end
-   
    assign rd = memory[a >> 2];
+   //assign rd = buffer;//memory[a >> 2];
 endmodule
